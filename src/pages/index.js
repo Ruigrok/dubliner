3c8c4d777242a5aaae9da7bd2f9dc084b62f6b1d
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2018-09-15 15:00:18 -0500

    adding basic boat info

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,8 @@ class IndexPage extends React.Component {
     this.state = {
       activeCategory: '',
       slideIndex: 0,
-      dimensions: 0
+      dimensions: 0,
+      tableLength: 0
     }
 
     this.openModal = this.openModal.bind(this);
@@ -25,11 +26,25 @@ class IndexPage extends React.Component {
 
   componentDidMount() {
     window.onresize = this.resize;
-    this.setState({ dimensions: (window.innerWidth / 4) - 2 })
+
+    let tableLength;
+    window.innerWidth >= 700 ? tableLength = 6 : tableLength = 3;
+
+    this.setState({
+      dimensions: (window.innerWidth / 4) - 2,
+      tableLength: tableLength
+    });
   }
 
   resize() {
-    this.setState({ dimensions: (window.innerWidth / 4) - 2 });
+    let tableLength;
+
+    window.innerWidth >= 700 ? tableLength = 6 : tableLength = 3;
+
+    this.setState({
+      dimensions: (window.innerWidth / 4) - 2,
+      tableLength: tableLength
+    });
   }
 
   openModal(categoryIndex, imageIndex) {
@@ -76,12 +91,12 @@ class IndexPage extends React.Component {
 
 
   render() {
-    const { activeCategory, slideIndex, dimensions } = this.state;
+    const { activeCategory, slideIndex, dimensions, tableLength } = this.state;
     const { data } = this.props;
 
     const categories = [
       {
-        name: 'Lagoon 420',
+        name: 'Exterior Views',
         images: data.exteriorImages.edges
       },
       {
@@ -97,6 +112,59 @@ class IndexPage extends React.Component {
         images: data.interiorImages.edges
       },
     ];
+
+
+    const info = [
+      {
+        header: 'Year',
+        data: '2007'
+      },
+      {
+        header: 'Location',
+        data: 'Grenadines'
+      },
+      {
+        header: 'Price',
+        data: '$365,000'
+      },
+      {
+        header: 'Length',
+        data: "42'"
+      },
+      {
+        header: 'Beam',
+        data: "24.6'"
+      },
+      {
+        header: 'Draft',
+        data: "4.3'"
+      },
+      {
+        header: 'Material',
+        data: 'Fiberglass'
+      },
+      {
+        header: 'Hull',
+        data: 'Catamaran'
+      },
+      {
+        header: 'Type',
+        data: 'Cruiser'
+      },
+      {
+        header: 'Rigging',
+        data: 'Masthead sloop'
+      },
+      {
+        header: 'Cabins',
+        data: 3
+      },
+      {
+        header: 'Condition',
+        data: 'Excellent'
+      },
+    ];
+
 
     return (
       <Layout /* location={this.props.location} */>
@@ -161,6 +229,151 @@ class IndexPage extends React.Component {
         </div>
 
         <div id="content">
+          <h2 style={{ textAlign: 'center', marginTop: '1.45rem' }}>Lagoon 420</h2>
+
+          <div style={{ margin: 20, display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+            <div style={{ minWidth: 100, maxWidth: 300 }}>
+              <h4>Overview:</h4>
+              <ul>
+                <li>Converted from hybrid in 2010</li>
+                <li>Owners Version</li>
+                <li>Fully Blue Water Equipped</li>
+                <li>Two Yanmar 40 hp diesels 1520 hrs approx each</li>
+                <li>SD 50 Saildrives</li>
+                <li>Three Cabins</li>
+                <li>Three Electric Flush Toilets</li>
+              </ul>
+            </div>
+            <div style={{ minWidth: 100, maxWidth: 300 }}>
+              <h4>Features:</h4>
+              <ul>
+                <li>750 watts of solar panels, Wind generator</li>
+                <li>EPIRB</li>
+                <li>Offshore life raft,</li>
+                <li>Onan Generator</li>
+                <li>Invertor</li>
+                <li>Four electric winches</li>
+                <li>Raymarine chart plotter with integrated radar and AIS</li>
+                <li>Four AC Units</li>
+                <li>Achilles Dinghy & 20hp electric start Honda Motor</li>
+              </ul>
+            </div>
+            <div style={{ minWidth: 100, maxWidth: 300 }}>
+              <h4>2018 Upgrades:</h4>
+              <ul>
+                <li>New Windlass & Anchor Chain</li>
+                <li>Four 200 amp Gel House Batterys</li>
+                <li>Mastervolt Battery Charger</li>
+                <li>Lots of tools and spare parts, pots, pans, dishes, and linens</li>
+              </ul>
+            </div>
+          </div>
+
+
+
+
+          <div style={{ margin: 10 }}>
+
+            {tableLength === 6 &&
+              <React.Fragment>
+                <table className='info-table'>
+                  <tbody>
+                    <tr>
+                      {info.slice(0, tableLength).map((item, itemIndex) => (
+                        <th>{item.header}</th>
+                      ))}
+                    </tr>
+                    <tr>
+                      {info.slice(0, tableLength).map((item, itemIndex) => (
+                        <td>{item.data}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+
+                <table className='info-table'>
+                  <tbody>
+                    <tr>
+                      {info.slice(tableLength).map((item, itemIndex) => (
+                        <th>{item.header}</th>
+                      ))}
+                    </tr>
+                    <tr>
+                      {info.slice(tableLength).map((item, itemIndex) => (
+                        <td>{item.data}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+              </React.Fragment>
+            }
+
+            {tableLength === 3 &&
+              <React.Fragment>
+                <table className='info-table'>
+                  <tbody>
+                    <tr>
+                      {info.slice(0, 3).map((item, itemIndex) => (
+                        <th>{item.header}</th>
+                      ))}
+                    </tr>
+                    <tr>
+                      {info.slice(0, 3).map((item, itemIndex) => (
+                        <td>{item.data}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+
+                <table className='info-table'>
+                  <tbody>
+                    <tr>
+                      {info.slice(3, 6).map((item, itemIndex) => (
+                        <th>{item.header}</th>
+                      ))}
+                    </tr>
+                    <tr>
+                      {info.slice(3, 6).map((item, itemIndex) => (
+                        <td>{item.data}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+
+                <table className='info-table'>
+                  <tbody>
+                    <tr>
+                      {info.slice(6, 9).map((item, itemIndex) => (
+                        <th>{item.header}</th>
+                      ))}
+                    </tr>
+                    <tr>
+                      {info.slice(6, 9).map((item, itemIndex) => (
+                        <td>{item.data}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+
+                <table className='info-table'>
+                  <tbody>
+                    <tr>
+                      {info.slice(9).map((item, itemIndex) => (
+                        <th>{item.header}</th>
+                      ))}
+                    </tr>
+                    <tr>
+                      {info.slice(9).map((item, itemIndex) => (
+                        <td>{item.data}</td>
+                      ))}
+                    </tr>
+                  </tbody>
+                </table>
+              </React.Fragment>
+            }
+          </div>
+
+
           {categories.map((category, categoryIndex) => (
             <div key={category.name}>
               <h2 style={{ textAlign: 'center', marginTop: '1.45rem' }}>{category.name}</h2>
@@ -207,7 +420,7 @@ class IndexPage extends React.Component {
             </div>
           ))}
         </div>
-      </Layout>
+      </Layout >
     );
   }
 }
